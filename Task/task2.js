9a770fd26b7b3b0ac34034317aea5bb53241a6ab
Sasha Istomin <sasaistomin42@gmail.com>
2026-02-19 17:46:07 +0200

    do tasks

---
+++ Task/task2.js
@@ -0,0 +1,2 @@
+let number = 30; console.log(number)
+number = 10; console.log(number);
---
+++ Task/task4.js
@@ -0,0 +1,7 @@
+user = {
+    name: "Sasha",
+    age: 17,
+    city: "Odessa"
+}
+
+console.log(`Name: ${user.name} \nAge: ${user.age} \nCity: ${user.city}`)
